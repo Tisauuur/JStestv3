c719c69baf5a2dd1cffeeae165cb3ba6d9b7868e
Tisauuur <128767187+Tisauuur@users.noreply.github.com>
2024-04-10 22:29:08 -0400

    subtract, multiply and divide functions added.

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 function add(...nums) {
     return nums.reduce((total, num) => total + num, 0);
-}
+};
+
+function subtract(...nums) {
+    return nums.reduce((total, num) => total - num, 0);
+};
+
+function multiply(...nums) {
+    return nums.reduce((total, num) => total * num);
+};
+
+function divide(...nums) {
+    return nums.reduce((total, num) => total / num);
+};
+
 
